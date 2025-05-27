ff13ee284a7e3418bb9077fe5f8b7127713e03a4
Daiyaan11 <daiyaaneasthorpe11@gmail.com>
2025-05-27 12:43:32 +0200

    edit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.collect import collect_tweets_by_topic
 
 def main():
     client = authenticate_v2()
-    topic = "#eskom OR #citypower OR #joburgwater OR #servicedelivery OR #watercrisis OR #loadshedding OR #roadmaintenance OR #joburgissues"
+    topic = "#eskom OR #citypower OR #joburgwater OR #servicedelivery OR #waterupdates OR #loadshedding OR #roadmaintenance OR #joburgissues"
     collect_tweets_by_topic(client, topic, filename="output/johannesburg_tweets.csv", limit=100)
 
 if __name__ == "__main__":
